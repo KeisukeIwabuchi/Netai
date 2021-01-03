6e7953037388f7439f314e08ff4cd1c6d5233a67
Keisuke Iwabuchi <cola.ice.puyo@gmail.com>
2021-01-04 02:34:40 +0900

    create http server

--- cmd/apps/netai/main.go
+++ cmd/apps/netai/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("寝たい")
+
+	fs := http.FileServer(http.Dir("web"))
+
+	http.Handle("/", fs)
+
+	http.ListenAndServe(":8888", nil)
 }
