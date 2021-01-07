512b1edd839e7fb49097b897133b38ea18068a45
Keisuke Iwabuchi <cola.ice.puyo@gmail.com>
2021-01-08 00:25:02 +0900

    Update transistor.go
    NPN型とPNP型で処理を変える

--- internal/apps/parts/transistor.go
+++ internal/apps/parts/transistor.go
@@ -1,22 +1,37 @@
 package parts
 
+// TransistorType is Enum for selecting the type of transistor
+type TransistorType int
+
+// NPN or PNP
+const (
+	NPN TransistorType = 1
+	PNP TransistorType = -1
+)
+
 // Transistor is electonic component
 type Transistor struct {
+	Type      TransistorType
+	Emitter   float64
 	Collector float64
 	Base      float64
 }
 
 // EmitterVoltage is return emitter voltage
 func (t Transistor) EmitterVoltage() float64 {
-	emitter := 0.0
+	if t.Type == PNP {
+		return t.Emitter
+	}
+
+	voltage := 0.0
 
 	if t.Base > 0 {
-		emitter = t.Base
+		voltage = t.Base
 
 		if t.Collector > 0 && t.Collector > t.Base {
-			emitter = t.Collector
+			voltage = t.Collector
 		}
 	}
 
-	return emitter
+	return voltage
 }
